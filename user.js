df8b9ed7a0e21ad70079ff6536344f047180aa67
Ser4ph4 <ser4ph4@proton.me>
2025-07-24 17:10:19 -0300

    Update user.js

--- user.js
+++ user.js
@@ -1,4 +1,5 @@
-/* Last Uptdate: 06/2025 */
+/* Source file made available under Mozilla Public License v. 2.0 See the main repository for updates as well as full license text. 
+   https://github.com/Godiesc/opera-gx */
 
 /* Default rules */
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
